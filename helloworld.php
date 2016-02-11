e7d0f166be5321c61814cb48ef097831d1c1369d
Vadim Kozlov <shikter@greeny.cs.tlu.ee>
2016-02-11 12:53:44 +0200

    next php code with numbers

--- helloworld.php
+++ helloworld.php
@@ -13,5 +13,23 @@
 	//for adding up strings .
 	echo $first_name." ".$last_name;
 
-
+	/*
+	< greater than
+	> smaller than
+	== equal
+	<= smaller than or equal
+	>= greater than or equal
+	!= not equal
+	*/
+	
+	$age = 24;
+	
+	//if age is smaller than or equal to 6, then you can sleep at lunchtime
+	if($age <= 6){
+		// it is ture
+		echo " Yes you can sleep at lunchtime";
+	}else{
+		//it is false
+		echo " no, you are at school";
+	}
 ?>
