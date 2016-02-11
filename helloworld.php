48d4d4e3161a58f84d6cac6885777eaf3c567249
Vadim Kozlov <shikter@greeny.cs.tlu.ee>
2016-02-11 12:43:03 +0200

    added first lines for PHP

--- helloworld.php
+++ helloworld.php
@@ -1,1 +1,17 @@
-Helloworld
+<?php
+	// helloworld
+
+
+	/* Variable starts with $, 
+	string has to use quotation 
+	marksline ends with semicolon */
+	$first_name = "Vadim";
+	$last_name = "Kozlov";
+	
+	//show code to browser
+	//for adding up numbers + sign
+	//for adding up strings .
+	echo $first_name." ".$last_name;
+
+
+?>
